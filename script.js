4b0c86c20f18b75c577d68d6062700d1146d9c0f
josue rojas <josuerojas.rojas@gmail.com>
2023-10-11 15:54:15 -0400

    secret

---
+++ script.js
@@ -0,0 +1,22 @@
+// on ready function using vanilla javascript
+document.addEventListener("DOMContentLoaded", function (event) { 
+    // do work
+  console.log("DOM fully loaded and parsed");
+});
+
+const PASSWORD = "LorenasBirthday";
+
+// function to check input box #password for correct password
+function verifyPassword () {
+  const password = document.getElementById("password");
+  const text = password.value;
+
+  if (text === PASSWORD) {
+    const hidden = document.querySelectorAll(".hide");
+    hidden.forEach(function (element) {
+      element.classList.remove("hide");
+    });
+  }
+
+  document.getElementById("secret").classList.add("hide");
+}
